ba6197ac00f0a8c040501c7336c051c481601975
Xcode User <xcode@fasts-macbook-pro-2.local>
2018-10-15 12:07:35 -0400

    Initial Commit

---
+++ Dhruv-Patel_57146-Midterm-Ex-01/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Dhruv-Patel_57146-Midterm-Ex-01
+//
+//  Created by Xcode User on 2018-10-15.
+//  Copyright © 2018 Sheridancollege. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
